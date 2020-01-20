82c1661ec3a4ed09948c2e866b2788b085b121fa
Koson Trachu <koson.trachu@gmail.com>
2020-01-21 05:01:08 +0700

    Create main.cpp

---
+++ Labs/main.cpp
@@ -0,0 +1,13 @@
+// Project1.cpp : Defines the entry point for the application.
+//
+
+#include "stdafx.h"
+#include "Project1.h"
+
+#define MAX_LOADSTRING 100
+
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrev, LPSTR lpCmdLine, int nCmdShow)
+{
+	MessageBox(NULL, _T("Hello World"), _T("Lab1"), MB_OK);
+	return 0;
+}
