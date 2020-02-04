63f7d1500e4d6e0e1d47f8985ace62980b927738
gingkasina <42264167+gingkasina@users.noreply.github.com>
2020-02-04 16:56:39 +0700

    ativity2

--- WIn32_Lab1.2/WIn32_Lab1.2/WIn32_Lab1.2.cpp
+++ WIn32_Lab1.2/WIn32_Lab1.2/WIn32_Lab1.2.cpp
@@ -6,42 +6,33 @@ int 	WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 	 	WNDCLASS wc;
 	  	HWND hwnd;
 		MSG msg;
-	 	/***************** 1. Define Windows class ****************************/
-    	wc.style = 0; // Class style
-	 	wc.lpfnWndProc = (WNDPROC)WndProc; // Window procedure address
-	 	wc.cbClsExtra = 0; // Class extra bytes
-	 	wc.cbWndExtra = 0; // Window extra bytes
-	 	wc.hInstance = hInstance; // Instance handle
-	 	wc.hIcon = LoadIcon(NULL, IDI_WINLOGO); // Icon handle
-	 	wc.hCursor = LoadCursor(NULL, IDC_ARROW); // Cursor handle
-	 	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1); // Background color
-		wc.lpszMenuName = NULL; // Menu name
-	 	wc.lpszClassName = "MyWndClass"; // WNDCLASS name
-	
-		 	/***************** 2. Register the Windows class **********************/
-	
+    	wc.style = 0; 
+	 	wc.lpfnWndProc = (WNDPROC)WndProc; 
+	 	wc.cbClsExtra = 0; 
+	 	wc.cbWndExtra = 0; 
+	 	wc.hInstance = hInstance; 
+	 	wc.hIcon = LoadIcon(NULL, IDI_WINLOGO); 
+	 	wc.hCursor = LoadCursor(NULL, IDC_ARROW); 
+	 	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1); 
+		wc.lpszMenuName = NULL;
+	 	wc.lpszClassName = "MyWndClass"; 
 		 	RegisterClass(&wc);
-	
-			/***************** 3. Create window **********************/
 		 	hwnd = CreateWindow(
-					"MyWndClass", // WNDCLASS name
-				"SDK Application", // Window title
-					WS_OVERLAPPEDWINDOW, // Window style
-					CW_USEDEFAULT, // Horizontal position
-			 		CW_USEDEFAULT, // Vertical position
-			 		CW_USEDEFAULT, // Initial width
-			 		CW_USEDEFAULT, // Initial height
-			 		HWND_DESKTOP, // Handle of parent window
-					NULL, // Menu handle
-					hInstance, // Application's instance handle
-			 		NULL // Window-creation data
-			;
-	
-		 	/***************** 4. Display the window **********************/
+					"MyWndClass",
+				"SDK Application", 
+					WS_OVERLAPPEDWINDOW, 
+					CW_USEDEFAULT, 
+			 		CW_USEDEFAULT, 
+			 		CW_USEDEFAULT, 
+			 		CW_USEDEFAULT, 
+			 		HWND_DESKTOP, 
+					NULL, 
+					hInstance, 
+			 		NULL 
+			);
 		 	ShowWindow(hwnd, nCmdShow);
 	 	UpdateWindow(hwnd);
 
-			/***************** 5. Message loop **********************/
 			while (GetMessage(&msg, NULL, 0, 0)) {
 		 		TranslateMessage(&msg);
 				DispatchMessage(&msg);
